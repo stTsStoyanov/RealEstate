d748695142273ca3e13c0fc269b2cea1ac4b6b76
unknown <st.ts.stoyanov@gmail.com>
2023-10-06 12:41:02 +0300

    update in in-memory-web-api data base

--- real-estate-web-page/src/app/in-memory-data.service.ts
+++ real-estate-web-page/src/app/in-memory-data.service.ts
@@ -11,55 +11,62 @@ export class InMemoryDataService implements InMemoryDbService{
   constructor() { }
 
   createDb(){
-    const apartments: Apartment[] =[
-      {name:'One bedroom Top location',
-      location:'Studentski grad',
-      id:1,
-      rooms:2,
-      price:88000,
-      service:false,
-      pictures:['../assets/images/id1Img1.jpg'],
-      description: "Epa toq e top chovek"},
-
-      {name:'One bedroom on hot price',
-      location:'Strelbishte',
-      id:2,
-      rooms:2,
-      price:108000,
-      service:false,
-      pictures:['../assets/images/id2Img1.jpg'],
-      description: "Leleeeeee skupoto batka, ama e mnogo dobro :D"},
-
-      {name:'One bedroom for fun people',
-      location:'Studentski grad',
-      id:3,
-      rooms:2,
-      price:68000,
-      service:false,
-      pictures:['../assets/images/id3Img1.jpg'],
-      description: "Studentka si 1-vi kurs, Studentski grad, nov resurs!!!"},
-
-      {name:'One bedroom on quite street',
-      location:'Lozenets',
-      id:4,
-      rooms:2,
-      price:168000,
-      service:true,
-      pictures:['../assets/images/id4Img1.jpg'],
-      description: "Male, male, male, a vie si mislite che Strelbishte e skup kvartal, ela da vidish Lozenets, top lokaciq, za top gazari, like pilencata ot Monako!!!"},
-
-      {name:'One bedroom best place in the Town',
-      location:'Malinova dolina',
-      id:5,
-      rooms:2,
-      price:98000,
-      service:false,
-      pictures:['../assets/images/id5Img1.jpg'],
-      description: "Smislen apartament, strahoten balans cena/kachestwo, zaslujava si da se vidi! Otoblenie na AC + wather heater, ima instalaciq za GAS, maze i parkomqsto, top izbor!"},
-    ]
+    const apartments: Apartment[] = [
+      {
+        name: 'One bedroom Top location',
+        location: 'Studentski grad',
+        id: 1,
+        rooms: 2,
+        price: 88000,
+        service: false,
+        pictures: ['../assets/images/id1Img1.jpg'],
+        description: 'Cozy one-bedroom apartment in the heart of Studentski grad, ideal for students or young professionals.'
+      },
+      {
+        name: 'One bedroom on hot price',
+        location: 'Strelbishte',
+        id: 2,
+        rooms: 2,
+        price: 108000,
+        service: false,
+        pictures: ['../assets/images/id2Img1.jpg'],
+        description: 'Affordable one-bedroom apartment in the popular Strelbishte area, great value for money.'
+      },
+      {
+        name: 'One bedroom for fun people',
+        location: 'Studentski grad',
+        id: 3,
+        rooms: 2,
+        price: 68000,
+        service: false,
+        pictures: ['../assets/images/id3Img1.jpg'],
+        description: 'Fun and vibrant one-bedroom apartment in Studentski grad, perfect for young students seeking a new adventure.'
+      },
+      {
+        name: 'One bedroom on quiet street',
+        location: 'Lozenets',
+        id: 4,
+        rooms: 2,
+        price: 168000,
+        service: true,
+        pictures: ['../assets/images/id4Img1.jpg'],
+        description: 'Peaceful one-bedroom apartment located on a quiet street in Lozenets, an ideal place for relaxation.'
+      },
+      {
+        name: 'One bedroom best place in the Town',
+        location: 'Malinova dolina',
+        id: 5,
+        rooms: 2,
+        price: 98000,
+        service: false,
+        pictures: ['../assets/images/id5Img1.jpg'],
+        description: 'Exceptional one-bedroom apartment in Malinova dolina, featuring AC, water heater, and a parking space.'
+      },
+    ];
 
     return {apartments};
-  }
+}
+
 
   genId(apartment: Apartment[]): number {
     return apartment.length > 0 ? Math.max(...apartment.map(apartmentId => apartmentId.id)) + 1 : 1;
